907568fb8af34264910938a7679edae6a3f319d0
Jakub Janczak <jakub.janczak@schibsted.pl>
2016-11-25 18:35:08 +0100

    Downstream lambdas support

--- lambdoku.js
+++ lambdoku.js
@@ -33,6 +33,11 @@ const setFunctionConfiguration = function(lambdaArn, config) {
     child_process.execSync(command, {encoding: 'utf8'});
 };
 
+const extractDownstreamLambdas = function(config) {
+    const configStringVal = config['DOWNSTREAM_LAMBDAS'] || '';
+    return configStringVal.length > 0 ? configStringVal.split(',') : [];
+};
+
 commander
     .version('1.0')
     .option('-a, --lambda <lambdaArn>', 'lambda to run operation on');
@@ -76,6 +81,41 @@ commander
             console.log(`No such env variable set ${envName}`);
             process.exit(1);
         }
+    });
+
+commander
+    .command('downstream:add <downstreamLambdaArn>')
+    .description('add lambda ARN as downstream to given lambda')
+    .action(function(downstreamLambdaArn) {
+        const lambdaArn = getLambdaArn(commander);
+        const config = getFunctionEnvVariables(lambdaArn);
+        const downstreamLambdas = extractDownstreamLambdas(config);
+        downstreamLambdas.push(downstreamLambdaArn);
+        config['DOWNSTREAM_LAMBDAS'] = downstreamLambdas.join(',');
+        setFunctionConfiguration(lambdaArn, config);
+    });
+
+commander
+    .command('downstream:remove <downstreamLambdaArn>')
+    .description('remove lambda ARN from given lambda')
+    .action(function(downstreamLambdaArn) {
+        const lambdaArn = getLambdaArn(commander);
+        const config = getFunctionEnvVariables(lambdaArn);
+        const downstreamLambdas = extractDownstreamLambdas(config);
+        downstreamLambdas.splice(downstreamLambdas.indexOf(downstreamLambdaArn), 1);
+        config['DOWNSTREAM_LAMBDAS'] = downstreamLambdas.join(',');
+        setFunctionConfiguration(lambdaArn, config);
+    });
+commander
+    .command('downstream')
+    .description('get downstream lambdas of given lambda')
+    .action(function() {
+        const lambdaArn = getLambdaArn(commander);
+        const config = getFunctionEnvVariables(lambdaArn);
+        const downstreamLambdas = extractDownstreamLambdas(config);
+        downstreamLambdas.forEach(function(lambdaArn) {
+             console.log(lambdaArn);
+        });
     });
 
 
