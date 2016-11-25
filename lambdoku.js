07354e5cacad2c0b6f8ee05a195e15da205bd575
Jakub Janczak <jakub.janczak@schibsted.pl>
2016-11-25 17:46:03 +0100

    setting configuration value for lambda

--- lambdoku.js
+++ lambdoku.js
@@ -20,6 +20,19 @@ const getLambdaArn = function(commander) {
     return lambdaArn;
 };
 
+const getFunctionConfiguration = function(lambdaArn) {
+    const command = `aws lambda get-function-configuration --function-name ${lambdaArn}`;
+    return JSON.parse(child_process.execSync(command, {encoding: 'utf8'})).Environment.Variables;
+};
+
+const setFunctionConfiguration = function(lambdaArn, config) {
+    const jsonConfig = JSON.stringify({
+        Variables: config
+    });
+    const command = `aws lambda update-function-configuration --function-name ${lambdaArn} --environment \'${jsonConfig}\'`;
+    child_process.execSync(command, {encoding: 'utf8'});
+};
+
 commander
     .version('1.0')
     .option('-a, --lambda <lambdaArn>', 'lambda to run operation on');
@@ -35,12 +48,20 @@ commander
     .command('config')
     .description('get env configuration for lambda')
     .action(function() {
-        const lambdaArn = getLambdaArn(commander);
-        const command = `aws lambda get-function-configuration --function-name ${lambdaArn}`
-        const jsonized = JSON.parse(child_process.execSync(command, {encoding: 'utf8'}));
-        for (const k in jsonized.Environment.Variables) {
-            console.log(`${k}='${jsonized.Environment.Variables[k]}'`)
+        const config = getFunctionConfiguration(getLambdaArn(commander));
+        for (const k in config) {
+            console.log(`${k}='${config[k]}'`)
         }
+    });
+
+commander
+    .command('config:set <envName> <envValue>')
+    .description('set configuration value of lambda')
+    .action(function(envName, envValue) {
+        const lambdaArn = getLambdaArn(commander);
+        const config = getFunctionConfiguration(lambdaArn);
+        config[envName] = envValue;
+        setFunctionConfiguration(lambdaArn, config);
     });
 
 commander.parse(process.argv);
